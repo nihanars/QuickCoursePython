5e3b3c7a3fbf7cc61dcff722e762b02b509429a3
nihan <nihanarslan33@gmail.com>
2021-12-03 20:19:34 +0300

    değişkenler belirlendi ve intro tanımlandı

--- budget.py
+++ budget.py
@@ -1,4 +1,15 @@
-import sys
 
-print("Bu program size ders notlarınıza göre bütçenizi gösterir.")
 
+name = str(input("Enter your name: "))
+
+totalExam = int(input("How much exams do you have in this year? : "))
+
+maxPoint = totalExam*50
+
+minPoint = totalExam*-50
+
+def intro(intro):
+    print("Hi ",name,"! This program helps you to calculate your points according to your exam results. You can get maximum ",
+maxPoint," points, and minimum ",minPoint," points in this year.",sep="",)
+
+intro(intro)
